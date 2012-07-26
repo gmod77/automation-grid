c9cac6427761237cbd9a428f04c997b5810c74f6
Gene Modin <gmodin@urbandaddy.com>
2012-07-26 15:42:32 -0400

    test: back to original

--- src/test/java/org/urbandaddy/tests/UD_Admin_Article_Weekender.java
+++ src/test/java/org/urbandaddy/tests/UD_Admin_Article_Weekender.java
@@ -6,44 +6,46 @@ import org.testng.annotations.Test;
 
 
 public class UD_Admin_Article_Weekender extends ITestCase {
-	
-	@Test (groups = "Smoke")
-	public void flow1(){loginUDAdmin();createArticleWeekender();
-	}
-	
-	@Override
-	public void beforeMethod() {
 
-	}
+@Test (groups = "Smoke")
+public void flow1(){
+	loginUDAdmin();
+	createArticleWeekender();
+}
 
-	@Override
-	public void afterMethod() {
-		// TODO Auto-generated method stub
+@Override
+public void beforeMethod() {
 
-	}
+}
 
-	@Override
-	public void beforeClass() {
-		// TODO Auto-generated method stub
+@Override
+public void afterMethod() {
+// TODO Auto-generated method stub
 
-	}
+}
 
-	@Override
-	public void afterClass() {
-		// TODO Auto-generated method stub
+@Override
+public void beforeClass() {
+// TODO Auto-generated method stub
 
-	}
+}
 
-	@Override
-	public void beforeTest() {
-		// TODO Auto-generated method stub
+@Override
+public void afterClass() {
+// TODO Auto-generated method stub
 
-	}
+}
 
-	@Override
-	public void afterTest() {
-		// TODO Auto-generated method stub
+@Override
+public void beforeTest() {
+// TODO Auto-generated method stub
 
-	}
+}
+
+@Override
+public void afterTest() {
+// TODO Auto-generated method stub
+
+}
 
 }
