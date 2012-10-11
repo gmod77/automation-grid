d04e4a86411968f34aa6b8f737b7cd2ea6b3035e
Scott Argenziano <sargenziano@urbandaddy.com>
2012-10-11 11:45:54 -0400

    Added env variable outputs again

--- src/test/java/org/urbandaddy/helpers/Comm/iSauceTest.java
+++ src/test/java/org/urbandaddy/helpers/Comm/iSauceTest.java
@@ -59,6 +59,7 @@ public class iSauceTest implements SauceOnDemandSessionIdProvider, SauceOnDemand
         System.out.println("HERE2> " + System.getenv("SELENIUM_BROWSER"));
         System.out.println("HERE2> " + System.getenv("SELENIUM_VERSION"));
         System.out.println("HERE2> " + System.getenv("SELENIUM_PLATFORM"));
+        System.out.println("HERE2> " + System.getenv("SELENIUM_DRIVER"));
 
         if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(key)) {
             authentication = new SauceOnDemandAuthentication(username, key);
